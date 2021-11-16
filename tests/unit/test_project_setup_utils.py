6b4644382ba31fbb63ddef66e10d0ad130e0ece1
Jake Beresford <jake.beresford@palmetto.com>
2021-11-15 19:43:32 -0500

    Finish testing and lint
    DATA-479

---
+++ tests/unit/test_project_setup_utils.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+import click
+import palm.project_setup_utils as project_setup_utils
+
+
+def test_has_env(tmp_path):
+    os.chdir(tmp_path)
+    assert not project_setup_utils.has_env()
+
+    Path('.env').touch()
+    assert project_setup_utils.has_env()
+
+
+def test_optionally_create_env(tmp_path, monkeypatch):
+    monkeypatch.setattr(click, 'confirm', lambda x: True)
+    os.chdir(tmp_path)
+    project_setup_utils.optionally_create_env()
+    assert Path('.env').exists()
+
+
+def test_make_executable(tmp_path):
+    os.chdir(tmp_path)
+    Path('file.sh').touch()
+    project_setup_utils.make_executable('file.sh')
+    assert os.access('file.sh', os.X_OK)
--- tests/unit/test_python_containerizer.py
+++ tests/unit/test_python_containerizer.py
@@ -1,12 +1,12 @@
 import os
 from pathlib import Path
 from palm.containerizer import PythonContainerizer
-import stat
 
 
 class MockContext:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
 
 # Clear abstract methods to allow testing ABC
 PythonContainerizer.__abstractmethods__ = set()
@@ -33,6 +33,7 @@ def test_has_requirements_txt(tmp_path, environment):
     Path('requirements.txt').touch()
     assert c.has_requirements_txt()
 
+
 def test_has_poetry(tmp_path, environment):
     os.chdir(tmp_path)
     ctx = MockContext(obj=environment)
@@ -42,8 +43,11 @@ def test_has_poetry(tmp_path, environment):
     Path('poetry.lock').touch()
     assert c.has_poetry()
 
+
 def test_run(tmp_path, environment):
-    templates_dir = Path(__file__).parents[2] / 'palm/plugins/core/templates/containerize'
+    templates_dir = (
+        Path(__file__).parents[2] / 'palm/plugins/core/templates/containerize'
+    )
 
     os.chdir(tmp_path)
     Path('.env').touch()
