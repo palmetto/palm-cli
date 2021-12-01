76465d75642e7668bccc5f91a59945070912c6ba
Jake Beresford <jake.beresford@palmetto.com>
2021-12-01 14:31:37 -0500

    Move palm lint and test into the core plugin, these are useful outside of this project!

--- palm/plugins/core/commands/cmd_test.py
+++ palm/plugins/core/commands/cmd_test.py
@@ -4,6 +4,6 @@ import click
 @click.command('test')
 @click.pass_context
 def cli(ctx):
-    """test"""
+    """Run tests for your application (pytest)"""
     click.echo("test command running!")
     ctx.obj.run_in_docker('pytest', {'PALM_TEST': True})
