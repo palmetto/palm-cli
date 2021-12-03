cb6f41504609f700e002be96e6c6a42af094bdce
Ethan Knox <ethan.m.knox@gmail.com>
2021-12-03 12:34:17 -0500

    test coverage

--- tests/unit/test_plugin_new.py
+++ tests/unit/test_plugin_new.py
@@ -4,8 +4,8 @@ from pathlib import Path
 def test_defined_template_files_exist():
     """Simple but a place bugs hide"""
 
-plugin_dir = Path("../../palm/plugins/core/templates/plugin/")
-    
-plugin_conf = yaml.safe_load((plugin_dir / "template-config.yaml").read_text())
-for filename in [list(f.keys())[0] for f in plugin_conf["files"]]:
-    assert (plugin_dir / filename).exists()
+    plugin_dir = Path("../../palm/plugins/core/templates/plugin/")
+        
+    plugin_conf = yaml.safe_load((plugin_dir / "template-config.yaml").read_text())
+    for filename in [list(f.keys())[0] for f in plugin_conf["files"]]:
+        assert (plugin_dir / filename).exists()
