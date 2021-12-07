da65e5f2b49be17ba58e626481a90f641ef0f3b3
Jake Beresford <jake.beresford@palmetto.com>
2021-12-07 10:58:42 -0500

    Improve boilerplate commands when scaffolding
    * Use pass_obj with environment argument name for improved readability
    * Include examples for run_in_docker and run_on_host

--- palm/plugins/core/templates/command_group/new_cmd_group.tpl.py
+++ palm/plugins/core/templates/command_group/new_cmd_group.tpl.py
@@ -9,9 +9,14 @@ def cli():
 
 {% for command in commands %}
 @cli.command()
-@click.pass_context
-def {{command}}(ctx):
+@click.pass_obj
+def {{command}}(environment):
     """{{command}}"""
     click.echo("{{command}} command running!")
+    command = "echo {{command}} running" 
+    # Run your command in docker:
+    # environment.run_in_docker(command)
+    # Or on the host:
+    # environment.run_on_host(command)
 
 {% endfor %}
