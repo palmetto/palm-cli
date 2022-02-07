8c9a914d4ba39f16f52d4de89ade522f7fde80a8
Jake Beresford <jake.beresford@palmetto.com>
2022-02-07 10:05:38 -0500

    Bump version and add release notes to changelog

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Palmetto Data Team'
 author = 'Palmetto Data Team'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.1.1'
 
 
 # -- General configuration ---------------------------------------------------
