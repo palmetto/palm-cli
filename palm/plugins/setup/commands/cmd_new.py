f526cd83ab8b3c446cddd5dc5b7fcac537df48a3
Jake Beresford <jake.beresford@palmetto.com>
2022-10-21 12:02:42 -0400

    Complete implementation of palm new

--- palm/plugins/setup/commands/cmd_new.py
+++ palm/plugins/setup/commands/cmd_new.py
@@ -1,8 +1,47 @@
 import click
-import subprocess
-
+import sys
+from pathlib import Path
 
 @click.command("new")
-def cli():
-    """Create a new project"""
-    subprocess.run(["echo", "palm new"], check=True)
+@click.option('--project_type', '-p', help='The type of project to create')
+@click.option('--template', '-t', help='Cookie cutter template to use')
+@click.pass_obj
+def cli(enviornment, project_type: str, template: str):
+    """Create a new project with cookiecutter"""
+    if project_type:
+        # Dictionary of standard project templates
+        # This should be moved to a config file!
+        cookie_cutters = {
+            'dbt': 'https://github.com/datacoves/cookiecutter-dbt',
+            'python': 'https://github.com/mariushelf/cookiecutter_python',
+            'python-package': 'https://github.com/Mgancita/cookiecutter-pypackage',
+        }
+        template = cookie_cutters.get(project_type)
+
+    if not template:
+        click.secho(f'No template defined for type {project_type}', fg='red')
+        template = click.prompt('Please enter a cookiecutter template url')
+
+    click.secho(f'Creating new {template} project', fg='green')
+
+    ex_code, _, _ = enviornment.run_on_host(f'cookiecutter {template}')
+
+    if ex_code == 0:
+        click.secho(f'Project created!', fg='green')
+    else:
+        click.secho(f'Failed to create project', fg='red')
+        sys.exit(1)
+
+    dir = click.prompt('Please enter the directory of the project')
+    if not Path(dir).exists():
+        click.secho(f'Project directory {dir} does not exist', fg='red')
+        click.secho(f'You will need to complete set up manually', fg='red')
+        sys.exit(1)
+
+    click.secho(f'Changing directory to {dir}', fg='green')
+    ex_code, _, _ = enviornment.run_on_host(f'cd {dir}')
+    enviornment.run_on_host('palm init')
+
+    containerize = click.confirm('Would you like to containerize this project?')
+    if containerize:
+        enviornment.run_on_host('palm containerize')
