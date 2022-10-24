41749c62a1366b4060c3a92a2cc4275c7bf1cf3c
Jake Beresford <jake.beresford@palmetto.com>
2022-10-24 15:55:46 -0400

    Remove junk code that was trying to be too smart
    - cookiecutter does way loads of great things, we shouldn't try to do more.
    If you want to containerize or palm init a newly generated project, you can
    do it manually without confusing this command

--- palm/plugins/setup/commands/cmd_new.py
+++ palm/plugins/setup/commands/cmd_new.py
@@ -10,14 +10,14 @@ from pathlib import Path
 @click.option('--template', '-t', help='Cookie cutter template to use')
 @click.option('--list', '-l', is_flag=True, help='List global cookiecutter templates')
 @click.pass_obj
-def cli(enviornment, global_template: str, template: str, list: bool = False):
+def cli(environment, global_template: str, template: str, list: bool = False):
     """Create a new project with cookiecutter"""
     if list:
-        list_templates(enviornment)
+        list_templates(environment)
         sys.exit(0)
 
     if global_template:
-        default_cookiecutters = enviornment.palm.config.get('default_cookiecutters', {})
+        default_cookiecutters = environment.palm.config.get('default_cookiecutters', {})
         template = default_cookiecutters.get(global_template)
 
     if not template:
@@ -26,7 +26,7 @@ def cli(enviornment, global_template: str, template: str, list: bool = False):
 
     click.secho(f'Creating new {template} project', fg='green')
 
-    ex_code, _, _ = enviornment.run_on_host(f'cookiecutter {template}')
+    ex_code, _, _ = environment.run_on_host(f'cookiecutter {template}')
 
     if ex_code == 0:
         click.secho(f'Project created!', fg='green')
@@ -34,23 +34,11 @@ def cli(enviornment, global_template: str, template: str, list: bool = False):
         click.secho(f'Failed to create project', fg='red')
         sys.exit(1)
 
-    dir = click.prompt('Please enter the directory of the project')
-    if not Path(dir).exists():
-        click.secho(f'Project directory {dir} does not exist', fg='red')
-        click.secho(f'You will need to complete set up manually', fg='red')
-        sys.exit(1)
-
-    if not (Path(dir) / '.palm').exists():
-        enviornment.run_on_host('cd {dir} && palm init')
-
-    containerize = click.confirm('Would you like to containerize this project?')
-    if containerize:
-        enviornment.run_on_host('cd {dir} && palm containerize')
 
 
-def list_templates(enviornment):
+def list_templates(environment):
     """List global cookiecutter templates"""
-    default_cookiecutters = enviornment.palm.config.get('default_cookiecutters', {})
+    default_cookiecutters = environment.palm.config.get('default_cookiecutters', {})
     click.secho('Global cookiecutter templates:', fg='green')
     for key in default_cookiecutters.keys():
         click.echo(f'  {key}: {default_cookiecutters[key]}')
