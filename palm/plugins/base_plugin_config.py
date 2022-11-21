6e7bc9093b9e28b3bbce98c94ca15452b4d91a29
Jake Beresford <jake.beresford@palmetto.com>
2022-11-21 09:54:09 -0500

    Use the main .palm/config.yml for plugin config
    Having separate yml files for each plugin config is messy,
    keeping all palm configurations in one place is much nicer.
    
    - Also update implementation to abstract base class

--- palm/plugins/base_plugin_config.py
+++ palm/plugins/base_plugin_config.py
@@ -1,27 +1,39 @@
-import yaml
-
 from abc import ABC, abstractmethod
+import click
 from pathlib import Path
+import yaml
 
 class BasePluginConfig(ABC):
     def __init__(self, plugin_name: str):
         self.plugin_name = plugin_name
-        self.config_path = Path.cwd() / ".palm" / f"{plugin_name}_config.yaml"
+        self.config_path = Path.cwd() / ".palm" / f"config.yaml"
 
     @abstractmethod
     def set_config(self) -> bool:
         pass
 
-    def get_config(self):
-        if not self._ensure_config():
+    def get_config(self) -> dict:
+        if not self.config_path.exists():
+            click.secho(
+                f"Config file not found at {self.config_path}, run palm init",
+                fg="red"
+            )
             return {}
 
-        return yaml.load(self.config_path.read_text(), Loader=yaml.FullLoader)
+        return self.read_config()
+
+    def read_config(self) -> dict:
+        palm_config = yaml.load(self.config_path.read_text(), Loader=yaml.FullLoader)
+        plugin_config = palm_config.get('plugin_config', {}).get(self.plugin_name, {})
+
+        if not plugin_config:
+            self.set_config()
+
+        return plugin_config
 
     def write_config(self, config: dict):
-        self.config_path.write_text(yaml.dump(config))
-
-    def _ensure_config(self) -> bool:
-        if not self.config_path.exists():
-            return self.set_config()
-        return True
+        palm_config = yaml.load(self.config_path.read_text(), Loader=yaml.FullLoader)
+        if not 'plugin_config' in palm_config.keys():
+            palm_config['plugin_config'] = {}
+        palm_config['plugin_config'][self.plugin_name] = config
+        self.config_path.write_text(yaml.dump(palm_config))
