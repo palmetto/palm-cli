eb8e6acf175561b1f7cb0f9142e77cea6822da55
Jake Beresford <jake.beresford@palmetto.com>
2023-05-15 14:52:16 -0400

    Add sensible defaults for log command
    - default to showing last 50 log lines

--- palm/plugins/multi_service/commands/cmd_logs.py
+++ palm/plugins/multi_service/commands/cmd_logs.py
@@ -1,14 +1,19 @@
+from typing import Optional
 import click
 
 
 @click.command("logs")
 @click.option("--container", "-c", help="Container to get logs from")
 @click.option("--follow", '-f', is_flag=True, help="Don't wrap command in /bin/bash -c")
+@click.option("-tail", '-n', default=50, help="Number of lines to tail, defaults to 50. Use 0 for all lines.")
 @click.pass_obj
-def cli(environment, container: str, follow: bool):
+def cli(environment, container: Optional[str], follow: bool, tail: int):
     """Tail logs from a container"""
 
-    cmd = ['docker logs']
+    if tail == 0:
+        tail = 'all'
+
+    cmd = ['docker logs', '-n', str(tail)]
 
     if follow:
         cmd.append('-f')
