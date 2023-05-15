a69e58c529d36454ca98795cb0ef9f87d1dc39d2
Jake Beresford <jake.beresford@palmetto.com>
2023-05-15 12:41:28 -0400

    Rename plugin and install plugin config

--- palm/plugins/multi_service/multi_service_plugin.py
+++ palm/plugins/multi_service/multi_service_plugin.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
+from palm.plugins.multi_service.multi_service_config import MultiServicePluginConfig
 from palm.plugins.base import BasePlugin
 
 MultiServicePlugin = BasePlugin(
     name="multi_service",
     command_dir=Path(__file__).parent / "commands",
+    config=MultiServicePluginConfig(),
 )
