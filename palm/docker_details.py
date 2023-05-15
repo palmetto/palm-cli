ab25c362e1ab2bb96ec329f7fb3babc3fffad4f9
Jake Beresford <jake.beresford@palmetto.com>
2023-05-15 15:35:45 -0400

    Add docs, types, and multi-service detection to docker details

--- palm/docker_details.py
+++ palm/docker_details.py
@@ -3,6 +3,8 @@ import yaml
 import subprocess
 from pathlib import Path
 
+from .palm_exceptions import NoRunningServicesError
+
 class DockerDetails:
     """Provides details about the docker environment to the palm config
     which can be used by commands and plugins that need to interact with
@@ -16,21 +18,33 @@ class DockerDetails:
         return yaml.safe_load(self.path.read_text())
 
     @property
-    def service_names(self):
+    def running_service_names(self) -> list[str]:
+        """Gets the names of currently running services for the current project.
+
+        Returns:
+            list[str]: The names of the running services
+        """
         services = list(self.config.get('services', {}).keys())
         service_names = []
         for service in services:
-            service_config = self.config.get('services', {}).get(service, {})
-            if service_config.get('container_name'):
-                service_names.append(service_config.get('container_name'))
-            else:
-                name = self._get_docker_service_name(service)
-                # Only append names of running services
-                if name:
-                    service_names.append(name)
+            name = self._get_running_service_name(service)
+            # Only append names of running services
+            if name:
+                service_names.append(name)
+        if not service_names:
+            raise NoRunningServicesError()
         return service_names
 
-    def _get_docker_service_name(self, service):
+    def _get_running_service_name(self, service: str) -> str:
+        """Gets the name of a running service based on the service name
+        defined in the docker-compose.yml file.
+
+        Args:
+            service (str): The service name defined in the docker-compose.yml
+
+        Returns:
+            str: The name of the running service
+        """
         cmd = 'docker ps -a --filter name={service} --format {{{{.Names}}}}'.format(service=service)
         result = subprocess.run(
           shlex.split(cmd),
@@ -40,3 +54,8 @@ class DockerDetails:
         if result.returncode != 0:
             raise Exception(result.stderr)
         return result.stdout.decode('utf-8').strip()
+
+    @property
+    def is_multi_service(self) -> bool:
+        """Checks if the docker-compose.yml has more than one service defined"""
+        return len(self.config.get('services', {})) > 1
