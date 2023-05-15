368b2e3c2036eca7425f4f403e0d8e7ab699460c
Jake Beresford <jake.beresford@palmetto.com>
2023-05-15 14:01:23 -0400

    Add new environment method for choice prompts
    Adds new UI to allow users to select from a list

--- palm/environment.py
+++ palm/environment.py
@@ -1,5 +1,7 @@
+from collections import OrderedDict
 import importlib
 from pathlib import Path
+from random import choice
 from typing import List, Optional, Tuple
 
 import click
@@ -104,3 +106,36 @@ class Environment:
         if plugin and plugin.config:
             return plugin.config.get()
         return None
+
+    def choice_prompt(self, prompt: str, options: List[str]) -> str:
+        """Prompt the user to select from a list of options
+
+        Args:
+            prompt (str): The prompt text to display to the user before the options
+            options (List[str]): The list of options to choose from
+
+        Returns:
+            str: The option the user selected
+        """
+        if len(options) == 1:
+            return options[0]
+
+        choice_map = OrderedDict((f'{i}', option) for i, option in enumerate(options, 1))
+        choices = choice_map.keys()
+        choice_lines = [f'{k}: {v}' for k, v in choice_map.items()]
+        default = 1
+
+        prompt = '\n'.join([
+            prompt,
+            '\n'.join(choice_lines),
+            f'Select from {len(choices)} options above',
+        ])
+
+        selection = click.prompt(
+            prompt,
+            type=click.Choice(choices),
+            show_choices=False,
+            default=default
+        )
+
+        return choice_map[selection]
