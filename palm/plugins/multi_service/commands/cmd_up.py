1d03c8325c3afa909e20eef91482253d20b0e012
Jake Beresford <jake.beresford@palmetto.com>
2023-05-16 11:03:14 -0400

    Default to deteched mode for compose up

--- palm/plugins/multi_service/commands/cmd_up.py
+++ palm/plugins/multi_service/commands/cmd_up.py
@@ -2,14 +2,14 @@ import click
 
 
 @click.command("up")
-@click.option("--detach", "-d", is_flag=True, help="Run containers in the background.")
+@click.option("--no-detach", "-nd", is_flag=True, help="Run containers in the background.")
 @click.option("--build", "-b", is_flag=True, help="Build images before starting containers.")
 @click.pass_obj
-def cli(environment, detach: bool, build: bool):
+def cli(environment, no_detach: bool, build: bool):
     """Bring up all services"""
 
     command = 'docker compose up'
-    if detach:
+    if not no_detach:
       command += ' -d'
     if build:
       command += ' --build'
