31b68168b9aff46946f526c4b9c5f05b794dee32
Jake Beresford <jake.beresford@palmetto.com>
2023-05-16 10:09:14 -0400

    Update build for multi-service support

--- palm/plugins/core/commands/cmd_build.py
+++ palm/plugins/core/commands/cmd_build.py
@@ -2,9 +2,18 @@ import click
 
 
 @click.command("build")
+@click.option("--service", "-s", help="Service to build (for multi-service projects)")
 @click.pass_obj
-def cli(environment):
-    """Rebuilds the image for the current working directory"""
+def cli(environment, service: str):
+    """Rebuilds the image for the current working directory.
 
-    command = f'docker compose build {environment.palm.image_name}'
+    For multi-service projects, you can specify a service to build with the -
+    -service flag. Otherwise, you will rebuild all services.
+    """
+
+    if service and environment.palm.is_multi_service:
+        command = f'docker compose build {service}'
+    else:
+        command = f'docker compose build'
+
     environment.run_on_host(command, check=True)
