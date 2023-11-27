4f05da4bb717733a8b665e47cf38317c4befe916
Jake Beresford <jake.beresford@palmetto.com>
2023-11-27 13:58:46 -0500

    Remove flags from down command. Anyone who understands those flags should just use the docker compose cli

--- palm/plugins/multi_service/commands/cmd_down.py
+++ palm/plugins/multi_service/commands/cmd_down.py
@@ -2,13 +2,10 @@ import click
 
 
 @click.command("down")
-@click.argument("flags", nargs=-1)
 @click.pass_obj
-def cli(environment, flags: tuple):
+def cli(environment):
     """Bring up all services"""
-    # This sucks. Don't commit this.
-    flags = [f"-{flag}" if len(flag) == 1 else f"--{flag}" for flag in flags]
-    command = f'docker compose down {" ".join(flags)}'
+    command = f'docker compose down'
 
     click.secho(f'Tearing down all services...', fg='yellow')
     environment.run_on_host(command, check=True)
